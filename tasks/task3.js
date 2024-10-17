d6165f8ef477593ce9b84b8cca39d51d2d0cc182
Mykyta1003 <kn23-m.trynko@nubip.edu.ua>
2024-10-17 16:30:52 +0300

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,12 @@
 // З масива людей(people) Вам потрібно знайти людину за іменем(name)
+// Приймає масив людей та ім'я, і шукає людину з таким ім'ям
 function findByName(people, name) {
-  // Ваш код
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].name === name) {
+      return people[i];
+    }
+  }
+  return undefined;
 }
 
 module.exports = findByName;
